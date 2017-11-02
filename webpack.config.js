337dfbae162a7c3d6cf30d863d152341c05f4bdc
Tom Ruttle <tom@tomruttle.com>
2017-11-02 11:06:09 +0200

    set up environment

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const SUPPORTED_BROWSERS = ['last 2 versions', 'ie 9', 'ie 10'];
+
+module.exports = (env) => ({
+  entry: {
+    'app-manager': path.join(__dirname, 'lib', 'app-manager.js'),
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    library: 'app-manager',
+  },
+  devtool: !env === 'prod' ? 'inline-source-map' : false,
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['env', { loose: true, modules: false, targets: { browsers: SUPPORTED_BROWSERS } }],
+              'flow',
+            ],
+            plugins: ['transform-class-properties'],
+          },
+        },
+      },
+    ],
+  },
+});
