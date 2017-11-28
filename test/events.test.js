843fad3671ca3205fc163a5b518db5e68d806857
Tom Ruttle <tom@tomruttle.com>
2017-11-28 15:35:05 +0000

    add unbind test

---
+++ test/events.test.js
@@ -0,0 +1,57 @@
+// @flow
+
+import { expect } from 'chai';
+
+import { WindowStub, HistoryStub } from '../lib/utils/stubs';
+import decorateHistory from '../lib/utils/decorate-history';
+
+import initAppManager from '../lib/app-manager';
+
+describe('events', () => {
+  function waitForIO() {
+    return new Promise((resolve) => setImmediate(resolve));
+  }
+
+  it('unbinds the statechange event listener if the init call fails', async () => {
+    const apps = {
+      APP_A: {
+        name: 'APP_A',
+        appPath: '/app-a/:entityId?',
+        display: ['SCRIPT_A'],
+      },
+    };
+
+    const config = {
+      apps,
+      slots: {},
+      scripts: {},
+    };
+
+    const windowStub = new WindowStub('/app-a');
+
+    const historystub = decorateHistory(windowStub, new HistoryStub());
+
+    const AppManager = initAppManager(windowStub, historystub);
+
+    const appManager = new AppManager(config);
+
+    expect(appManager._status).to.equals('DEFAULT');
+
+    historystub.pushState({}, null, '/app-b');
+
+    await waitForIO();
+
+    const success = await appManager.init();
+
+    expect(success).to.be.false;
+    expect(appManager._currentAppName).to.be.null;
+    expect(appManager._status).to.equals('ERROR');
+
+    historystub.pushState({}, null, '/app-a');
+
+    await waitForIO();
+
+    expect(appManager._currentAppName).to.be.null;
+    expect(appManager._status).to.equals('ERROR');
+  });
+});
