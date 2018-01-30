f3d408f7cf1a25620b75606e436345a891651f89
Tom Ruttle <tom@tomruttle.com>
2018-01-30 17:26:09 +0000

    add event log to footer

--- examples/simple-react/src/components/footer-app.jsx
+++ examples/simple-react/src/components/footer-app.jsx
@@ -1,11 +1,53 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
 
-export default function FooterApp() {
-  return (
-    <div>
-      <h3>Footer App</h3>
-    </div>
+export type EventType = {
+  id: string,
+  data: string,
+}
+
+type FooterAppProps = {
+  children: (updateEvents: (event: EventType) => void) => void,
+};
+
+type FooterAppState = {
+  events: Array<EventType>,
+};
+
+const Footer = styled.div`
+  margin-top: 50px;
+`;
+
+const ScrollableMenu = styled.div`
+  height: 160px;
+`;
+
+export default class FooterApp extends Component<FooterAppProps, FooterAppState> {
+  state = {
+    events: [],
+  };
+
+  componentDidMount = () => {
+    this.props.children(this.updateEvents);
+  }
+
+  updateEvents = (event: EventType) => {
+    this.setState((prevState) => ({ events: [event, ...prevState.events] }));
+  }
+
+  render = () => (
+    <Footer>
+      <h3>Events</h3>
+
+      <ScrollableMenu className="pure-menu pure-menu-scrollable">
+        <ul className="pure-menu-list">
+
+          {this.state.events.map((event: EventType, index: number) => <li className="pure-menu-item" key={event.id}>{this.state.events.length - index}. {event.data}</li>)}
+
+        </ul>
+      </ScrollableMenu>
+    </Footer>
   );
 }
--- examples/simple-react/src/guests/footer-script.jsx
+++ examples/simple-react/src/guests/footer-script.jsx
@@ -2,28 +2,64 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import shortId from 'shortid';
 
-import type { ScriptVersion4Type } from '../../../../lib/index';
+import type { ScriptVersion4Type, StatusType, AppType, EventTitleType } from '../../../../lib/index';
+import type { EventType } from '../components/footer-app';
+
 import render from '../utils/render';
 
 import FooterApp from '../components/footer-app';
 
 class FooterScript implements ScriptVersion4Type {
+  _currentAppName: ?string = null;
+  _updateEventsCallback: ?(event: EventType) => void = null;
+
   version = 4;
 
-  hydrate = async (container: Element) => {
-    const app = <FooterApp />;
-    return render(app, container);
+  _getApp = () => (
+    <FooterApp>
+      {(updateEventsCallback) => {
+        this._updateEventsCallback = updateEventsCallback;
+      }}
+    </FooterApp>
+  );
+
+  hydrate = async (container: Element, currentApp: AppType) => {
+    this._currentAppName = currentApp.name;
+    return render(this._getApp(), container);
   }
 
-  mount = async (container: Element) => {
-    const app = <FooterApp />;
-    return render(app, container);
+  mount = async (container: Element, eventTitle: EventTitleType, currentApp: AppType) => {
+    this._currentAppName = currentApp.name;
+    return render(this._getApp(), container);
   }
 
-  onStateChange = async () => {};
+  onStateChange = async (eventTitle: EventTitleType, currentApp: AppType) => {
+    const eventId = shortId.generate();
 
-  onUpdateStatus = async () => {};
+    if (typeof this._updateEventsCallback === 'function') {
+      this._updateEventsCallback({
+        id: eventId,
+        data: `${this._currentAppName ? `${this._currentAppName} UNMOUNTED AND ` : ''}${currentApp.name} MOUNTED`,
+      });
+    }
+
+    this._currentAppName = currentApp.name;
+  };
+
+  onUpdateStatus = async (status: StatusType, currentApp: AppType) => {
+    const eventId = shortId.generate();
+
+    if (status === 'ERROR' && typeof this._updateEventsCallback === 'function') {
+      this._updateEventsCallback({
+        id: eventId,
+        data: 'ERROR OCCURRED',
+      });
+    }
+
+    this._currentAppName = currentApp.name;
+  };
 
   unmount = async (container: Element) => {
     ReactDOM.unmountComponentAtNode(container);
