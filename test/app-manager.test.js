a9316b356c2c5b24d674be90844564f6ac1cde5b
Tom Ruttle <tom@tomruttle.com>
2018-03-01 21:51:51 +0000

    solidify code in app-manager

--- test/app-manager.test.js
+++ test/app-manager.test.js
@@ -4,14 +4,13 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 
 import initAppManager from '../lib/app-manager';
-import { awaitEvent } from './utils';
 
 describe('app-manager', () => {
-  const windowStub = {
+  const appManager = initAppManager({
     document: {
       querySelector() { return (true: any); },
     },
-  };
+  });
 
   describe('multi-step browser test', () => {
     const mockA = {
@@ -58,18 +57,19 @@ describe('app-manager', () => {
       },
     };
 
-    const appManager = initAppManager(windowStub);
+    const onError = sinon.spy();
 
-    let _stateChanger;
+    let stateChanger;
 
     before(() => {
-      _stateChanger = appManager(config);
+      stateChanger = appManager(config, { onError });
     });
 
     it('correctly initialises app manager with first fragment', async () => {
-      await _getRunningStateChange();
+      const proceed = await stateChanger({ resource: '/app-a' });
 
-      expect((_getState(): any).app.name).to.equals('APP_A');
+      expect(proceed).to.be.true;
+      expect(stateChanger.getState().app.name).to.equals('APP_A');
 
       expect(mockA.hydrate.callCount).to.equals(1);
       expect(mockA.render.callCount).to.equals(0);
@@ -85,11 +85,10 @@ describe('app-manager', () => {
     });
 
     it('browses to new app', async () => {
-      windowStub.history.pushState(null, null, '/app-b');
+      const proceed = await stateChanger({ resource: '/app-b' });
 
-      await _getRunningStateChange();
-
-      expect((_getState(): any).app.name).to.equals('APP_B');
+      expect(proceed).to.be.true;
+      expect(stateChanger.getState().app.name).to.equals('APP_B');
 
       expect(mockA.hydrate.callCount).to.equals(1);
       expect(mockA.render.callCount).to.equals(0);
@@ -105,11 +104,10 @@ describe('app-manager', () => {
     });
 
     it('moves within an app', async () => {
-      windowStub.history.pushState(null, null, '/app-b/next');
+      const proceed = await stateChanger({ resource: '/app-b/next' });
 
-      await _getRunningStateChange();
-
-      expect((_getState(): any).app.name).to.equals('APP_B');
+      expect(proceed).to.be.true;
+      expect(stateChanger.getState().app.name).to.equals('APP_B');
 
       expect(mockA.hydrate.callCount).to.equals(1);
       expect(mockA.render.callCount).to.equals(0);
@@ -125,11 +123,10 @@ describe('app-manager', () => {
     });
 
     it('moves back within an app', async () => {
-      windowStub.history.go(-1);
+      const proceed = await stateChanger({ resource: '/app-b' });
 
-      await _getRunningStateChange();
-
-      expect((_getState(): any).app.name).to.equals('APP_B');
+      expect(proceed).to.be.true;
+      expect(stateChanger.getState().app.name).to.equals('APP_B');
 
       expect(mockA.hydrate.callCount).to.equals(1);
       expect(mockA.render.callCount).to.equals(0);
@@ -145,11 +142,10 @@ describe('app-manager', () => {
     });
 
     it('moves back to the old app', async () => {
-      windowStub.history.back();
+      const proceed = await stateChanger({ resource: '/app-a' });
 
-      await _getRunningStateChange();
-
-      expect((_getState(): any).app.name).to.equals('APP_A');
+      expect(proceed).to.be.true;
+      expect(stateChanger.getState().app.name).to.equals('APP_A');
 
       expect(mockA.hydrate.callCount).to.equals(1);
       expect(mockA.render.callCount).to.equals(1);
@@ -165,11 +161,10 @@ describe('app-manager', () => {
     });
 
     it('moves forward again', async () => {
-      windowStub.history.forward();
+      const proceed = await stateChanger({ resource: '/app-b' });
 
-      await _getRunningStateChange();
-
-      expect((_getState(): any).app.name).to.equals('APP_B');
+      expect(proceed).to.be.true;
+      expect(stateChanger.getState().app.name).to.equals('APP_B');
 
       expect(mockA.hydrate.callCount).to.equals(1);
       expect(mockA.render.callCount).to.equals(1);
@@ -185,11 +180,10 @@ describe('app-manager', () => {
     });
 
     it('If a fragment does not have a loadScript function, emit a missing-scripts event', async () => {
-      windowStub.history.pushState(null, null, '/app-c');
+      const proceed = await stateChanger({ resource: '/app-c' });
 
-      await awaitEvent(events, 'am-missing-scripts');
-
-      expect((_getState(): any).app.name).to.equals('APP_C');
+      expect(proceed).to.be.false;
+      expect(stateChanger.getState().app.name).to.equals('APP_C');
 
       expect(mockA.hydrate.callCount).to.equals(1);
       expect(mockA.render.callCount).to.equals(1);
@@ -209,30 +203,189 @@ describe('app-manager', () => {
       expect(mockC.unmount.callCount).to.equals(0);
       expect(mockC.onUpdateStatus.callCount).to.equals(0);
     });
+
+    it('Expect there to have been no errors', () => {
+      expect(onError.called).to.be.false;
+    });
   });
 
   describe('errors', () => {
     it('emits an error event if app tries to load missing fragment', async () => {
       const config = {
         apps: {
-          APP_A: { appPath: '/app-a', fragment: 'SCRIPT_A' },
-          APP_B: { appPath: '/app-b', fragment: 'MISSING' },
+          APP_A: { appPath: '/app-a', fragment: 'MISSING' },
         },
-        fragments: {
-          SCRIPT_A: { slot: 'MAIN' },
-        },
+        fragments: {},
         slots: {},
       };
 
-      const onErrorSpy = sinon.spy();
+      const stateChanger = appManager(config);
 
-      const appManager = initAppManager(windowStub);
+      try {
+        await stateChanger({ resource: '/app-a' });
+        throw new Error('Should not get here.');
+      } catch (err) {
+        expect(err.code).to.equals('missing_fragment');
+      }
+    });
+  });
 
-      appManager(config);
+  describe('Lifecycle', () => {
+    function getSpyScript() {
+      return {
+        version: 5,
+        onStateChange: sinon.spy(),
+        onUpdateStatus: sinon.spy(),
+        hydrate: sinon.spy(),
+        render: sinon.spy(),
+        unmount: sinon.spy(),
+      };
+    }
 
-      const err = onErrorSpy.args[0][0];
+    describe('setup page', () => {
+      it('hydrates a script with no errors', async () => {
+        const appScript = getSpyScript();
 
-      expect(err.code).to.equals('invalid_slots');
+        const config = {
+          apps: {
+            APP_A: { appPath: '/app-a', fragment: 'SCRIPT_A' },
+          },
+          fragments: {
+            SCRIPT_A: { slot: 'MAIN', loadScript() { return appScript; } },
+          },
+          slots: {
+            MAIN: { querySelector: null },
+          },
+        };
+
+        const stateChanger = appManager(config);
+
+        await stateChanger({ resource: '/app-a' });
+
+        expect(appScript.onStateChange.callCount).to.equals(0);
+        expect(appScript.render.callCount).to.equals(0);
+        expect(appScript.hydrate.callCount).to.equals(1);
+        expect(appScript.unmount.callCount).to.equals(0);
+        expect(appScript.onUpdateStatus.callCount).to.equals(2);
+
+        const [loadingCall, defaultCall] = appScript.onUpdateStatus.args;
+        expect(loadingCall[0].status).to.equals('LOADING');
+        expect(defaultCall[0].status).to.equals('DEFAULT');
+      });
+
+      it('handles errors on hydrate', async () => {
+        const headerScript = getSpyScript();
+
+        const errorScript = {
+          version: 5,
+          onStateChange: sinon.spy(),
+          onUpdateStatus: sinon.spy(),
+          hydrate: sinon.stub().throws(),
+          render: sinon.spy(),
+          unmount: sinon.spy(),
+        };
+
+        const config = {
+          apps: {
+            APP_A: { appPath: '/app-a', fragments: ['ERROR_FRAGMENT', 'HEADER_FRAGMENT'] },
+          },
+          fragments: {
+            ERROR_FRAGMENT: { slot: 'APP_SLOT', loadScript() { return errorScript; } },
+            HEADER_FRAGMENT: { slot: 'HEADER_SLOT', loadScript() { return headerScript; } },
+          },
+          slots: {
+            APP_SLOT: { querySelector: null },
+            HEADER_SLOT: { querySelector: null },
+          },
+        };
+
+        const stateChanger = appManager(config);
+
+        await stateChanger({ resource: '/app-a' });
+
+        expect(errorScript.onStateChange.callCount).to.equals(0);
+        expect(errorScript.render.callCount).to.equals(0);
+        expect(errorScript.hydrate.callCount).to.equals(1);
+        expect(errorScript.unmount.callCount).to.equals(1);
+        expect(errorScript.onUpdateStatus.callCount).to.equals(2);
+
+        const [errorLoadingCall, errorErrorCall] = errorScript.onUpdateStatus.args;
+        expect(errorLoadingCall[0].status).to.equals('LOADING');
+        expect(errorErrorCall[0].status).to.equals('ERROR');
+
+        expect(headerScript.onStateChange.callCount).to.equals(0);
+        expect(headerScript.render.callCount).to.equals(0);
+        expect(headerScript.hydrate.callCount).to.equals(1);
+        expect(headerScript.unmount.callCount).to.equals(0);
+        expect(headerScript.onUpdateStatus.callCount).to.equals(3);
+
+        const [loadingCall, errorCall, defaultCall] = headerScript.onUpdateStatus.args;
+        expect(loadingCall[0].status).to.equals('LOADING');
+        expect(errorCall[0].status).to.equals('ERROR');
+        expect(defaultCall[0].status).to.equals('DEFAULT');
+      });
+    });
+
+    describe('update page', () => {
+      it('handles errors on update', async () => {
+        const headerScript = getSpyScript();
+
+        const errorScript = {
+          version: 5,
+          onStateChange: sinon.stub().throws(),
+          onUpdateStatus: sinon.spy(),
+          hydrate: sinon.spy(),
+          render: sinon.spy(),
+          unmount: sinon.spy(),
+        };
+
+        const config = {
+          apps: {
+            APP_A: { appPath: '/app-a', fragments: ['APP_FRAGMENT', 'HEADER_FRAGMENT'] },
+            APP_B: { appPath: '/app-b', fragments: ['ERROR_FRAGMENT', 'HEADER_FRAGMENT'] },
+          },
+          fragments: {
+            APP_FRAGMENT: { slot: 'APP_SLOT', loadScript() { return errorScript; } },
+            ERROR_FRAGMENT: { slot: 'APP_SLOT', loadScript() { return getSpyScript(); } },
+            HEADER_FRAGMENT: { slot: 'HEADER_SLOT', loadScript() { return headerScript; } },
+          },
+          slots: {
+            APP_SLOT: { querySelector: null },
+            HEADER_SLOT: { querySelector: null },
+          },
+        };
+
+        const stateChanger = appManager(config);
+
+        await stateChanger({ resource: '/app-a' });
+
+        await stateChanger({ resource: '/app-b' });
+
+        expect(errorScript.onStateChange.callCount).to.equals(1);
+        expect(errorScript.render.callCount).to.equals(0);
+        expect(errorScript.hydrate.callCount).to.equals(1);
+        expect(errorScript.unmount.callCount).to.equals(1);
+        expect(errorScript.onUpdateStatus.callCount).to.equals(4);
+
+        const [errorLoadingCall1, errorDefaultCall, errorLoadingCall2, errorErrorCall] = errorScript.onUpdateStatus.args;
+        expect(errorLoadingCall1[0].status).to.equals('LOADING');
+        expect(errorDefaultCall[0].status).to.equals('DEFAULT');
+        expect(errorLoadingCall2[0].status).to.equals('LOADING');
+        expect(errorErrorCall[0].status).to.equals('ERROR');
+
+        expect(headerScript.onStateChange.callCount).to.equals(1);
+        expect(headerScript.render.callCount).to.equals(0);
+        expect(headerScript.hydrate.callCount).to.equals(1);
+        expect(headerScript.unmount.callCount).to.equals(0);
+        expect(headerScript.onUpdateStatus.callCount).to.equals(5);
+
+        const [loadingCall1, defaultCall1, loadingCall2, errorCall, defaultCall2] = headerScript.onUpdateStatus.args;
+        expect(loadingCall1[0].status).to.equals('LOADING');
+        expect(defaultCall1[0].status).to.equals('DEFAULT');
+        expect(loadingCall2[0].status).to.equals('LOADING');
+        expect(errorCall[0].status).to.equals('ERROR');
+        expect(defaultCall2[0].status).to.equals('DEFAULT');
+      });
     });
   });
 });
