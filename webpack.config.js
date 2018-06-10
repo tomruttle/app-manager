743802b1ebb7dbee1934c2e28979fd9bbf2ac53d
Tom Ruttle <tom@tomruttle.com>
2018-06-10 21:04:36 +0200

    simplify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,11 @@
 const path = require('path');
 
-const SUPPORTED_BROWSERS = ['>0.25%', 'not ie 11', 'not op_mini all'];
-
-module.exports = (env) => ({
-  entry: {
-    'app-manager': ['babel-polyfill', path.join(__dirname, 'lib', 'index.js')],
-  },
+module.exports = {
+  entry: path.join(__dirname, 'es5', 'index.js'),
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].js',
+    filename: 'app-manager.js',
     library: 'AppManager',
     libraryTarget: 'umd',
   },
-  devtool: !env === 'prod' ? 'inline-source-map' : false,
-  node: {
-    console: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['env', { loose: true, modules: false, targets: { browsers: SUPPORTED_BROWSERS } }],
-              'flow',
-            ],
-            plugins: ['transform-object-rest-spread', 'transform-async-generator-functions', 'transform-class-properties', ['transform-builtin-extend', { globals: ['Error'] }]],
-          },
-        },
-      },
-    ],
-  },
-});
+};
