7a88f6b745f02ece64df2b9cd8781bbf91439ad4
Tom Ruttle <tom@tomruttle.com>
2018-06-10 13:52:49 +0200

    emit an event if there's no route

--- examples/microservices/main/src/client/index.js
+++ examples/microservices/main/src/client/index.js
@@ -8,4 +8,9 @@ import config, { options } from '../common/config';
 
 const events = new EventEmitter();
 
+events.on('hc-no-route', ({ resource }) => {
+  console.log(`navigating to ${resource}`);
+  window.location.href = `${window.location.origin}${resource}`;
+});
+
 appManager(config, events, options);
