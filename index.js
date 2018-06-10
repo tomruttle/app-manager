a1bf106a65886b0f7fa62712ab08a146ca101dd0
Tom Ruttle <tom@tomruttle.com>
2018-06-10 20:05:05 +0200

    export everything from module root

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ var appManager = require('./es5/index');
 
 module.exports = appManager.default;
 
+module.exports.eventTitles = appManager.eventTitles;
 module.exports.statuses = appManager.statuses;
 module.exports.levels = appManager.levels;
+module.exports.appManager = appManager.appManager;
